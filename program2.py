7ff14792d1bff749060b831cb76d904672929d3f
Jaswanth <palletijaswanth@gmail.com>
2024-11-11 15:38:42 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -1,10 +1,18 @@
 class Solution(object):
     def romanToInt(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        pass
-
-
-
+        roman_map = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+        result = 0
+        for i in range(len(s)):
+            if i < len(s) - 1 and roman_map[s[i]] < roman_map[s[i + 1]]:
+                result -= roman_map[s[i]]
+            else:
+                result += roman_map[s[i]]
+        return result
